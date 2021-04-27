70b52905631f01ca943c12d7011bf3224e51d4a8
ytx <ytx1991@gmail.com>
2021-04-27 12:13:32 -0700

    Add files via upload

---
+++ run_mtail.sh
@@ -0,0 +1,2 @@
+watch -n 30 /data2/chia-blockchain/venv/bin/chia farm summary
+./mtail --progs ./ --logs .chia/mainnet/log/debug.log --logs .chia/mainnet/log/farmer_summary.log --poll_interval 3000ms
