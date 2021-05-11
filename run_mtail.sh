178a26d02dd29f18a1ab880f1caee9dab53f7d4d
Kronus91 <ytx1991@gmail.com>
2021-05-11 14:37:12 -0700

    Run mtail in the backend

--- run_mtail.sh
+++ run_mtail.sh
@@ -1,2 +1,2 @@
-watch -n 30 ~/chia-blockchain/venv/bin/chia farm summary
-./mtail --progs ./ --logs .chia/mainnet/log/debug.log --logs .chia/mainnet/log/farmer_summary.log --poll_interval 3000ms
+nohup watch -n 30 ~/chia-blockchain/venv/bin/chia farm summary &
+nohup ./mtail --progs ./ --logs .chia/mainnet/log/debug.log --logs .chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
