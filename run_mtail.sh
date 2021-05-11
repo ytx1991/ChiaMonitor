b693c2eff59122f4ec648c9bf503933bc4d5cc69
Kronus91 <ytx1991@gmail.com>
2021-05-11 15:20:59 -0700

    Use abs path

--- run_mtail.sh
+++ run_mtail.sh
@@ -1,2 +1,2 @@
 nohup watch -n 30 ~/chia-blockchain/venv/bin/chia farm summary &
-nohup ./mtail --progs ./ --logs .chia/mainnet/log/debug.log --logs .chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
+nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
