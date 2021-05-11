a6735e64a71575880f050f8db6afbd2ec712c1ca
Kronus91 <ytx1991@gmail.com>
2021-05-11 15:27:34 -0700

    Fix chia farm log saving

--- run_mtail.sh
+++ run_mtail.sh
@@ -1,2 +1,2 @@
-nohup watch -n 30 ~/chia-blockchain/venv/bin/chia farm summary &
+nohup watch -n 30 "~/chia-blockchain/venv/bin/chia farm summary > ~/.chia/mainnet/log/farmer_summary.log" &
 nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ~/.chia/mainnet/log/farmer_summary.log --poll_interval 3000ms &
