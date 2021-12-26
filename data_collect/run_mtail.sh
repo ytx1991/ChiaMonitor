9b6fc8bb69ddcc399e278421c9e0528cf7f85956
ytx1991 <ytx1991@gmail.com>
2021-12-25 20:05:21 -0800

    add new line for linux wallet

--- data_collect/run_mtail.sh
+++ data_collect/run_mtail.sh
@@ -3,7 +3,7 @@
 fingerprint=Your_Wallet_Fingerprint
 chmod +x merge.sh
 nohup watch -n 30 "../venv/bin/chia farm summary > farmer_summary.log" &
-nohup watch -n 30 "../venv/bin/chia wallet show -f $fingerprint > wallet.log && grep  ' ' wallet.log | tr '\n' ' ' > one_line_wallet.log" &
+nohup watch -n 30 "../venv/bin/chia wallet show -f $fingerprint > wallet.log && grep  ' ' wallet.log | tr '\n' ' ' > one_line_wallet.log && echo -e '\n' >>  one_line_wallet.log" &
 nohup watch -n 30 "../venv/bin/chia plotnft show -f $fingerprint > nft.log && awk -v RS=Wallet '{print > ("nft-" NR ".log")}' nft.log " &
 nohup watch -n 30 "./merge.sh" &
 nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ./"*.log" --poll_interval 1000ms &
