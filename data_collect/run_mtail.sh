8a8ebeab7c4d3b9981c698b15e048cc313f6d127
ytx1991 <ytx1991@gmail.com>
2023-02-20 20:13:30 -0800

    Add GPU ploting support for Ubuntu/CN

--- data_collect/run_mtail.sh
+++ data_collect/run_mtail.sh
@@ -1,8 +1,9 @@
 # ~/.chia/mainnet/log/ is where your Chia logs saved.
 # Replace them if they are different on your host
-fingerprint=Your_Wallet_Fingerprint
+fingerprint=2637045125
 chmod +x merge.sh
 nohup watch -n 30 "../venv/bin/chia farm summary > farmer_summary.log" &
+nohup watch -n 30 "../venv/bin/chia rpc farmer get_harvesters_summary | python3 -c \"import sys, json; raw=' '.join(sys.stdin); res=json.loads(raw[raw.find('{'):]); [print(h) for h in res['harvesters']]\" > harvester_summary.log" &
 nohup watch -n 30 "../venv/bin/chia wallet show -f $fingerprint > wallet.log && grep  ' ' wallet.log | tr '\n' ' ' > one_line_wallet.log && echo -e '\n' >>  one_line_wallet.log" &
 nohup watch -n 30 "../venv/bin/chia plotnft show -f $fingerprint > nft.log && awk -v RS=Wallet '{print > ("nft-" NR ".log")}' nft.log " &
 nohup watch -n 30 "./merge.sh" &
